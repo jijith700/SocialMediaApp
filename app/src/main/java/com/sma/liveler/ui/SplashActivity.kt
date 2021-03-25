837faabb7ab7dd465124a0b34cb8303675e30462
Jijith Janardanan <jijith700@gmail.com>
2021-03-25 15:48:11 +0530

    Splash, sign-in and registration design changed.<Jijith>

--- app/src/main/java/com/sma/liveler/ui/SplashActivity.kt
+++ app/src/main/java/com/sma/liveler/ui/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.sma.liveler.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.sma.liveler.R
 import com.sma.liveler.ui.home.HomeActivity
@@ -13,14 +14,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        if (Utils.loadPreferenceString(this, getString(R.string.token)).isNotEmpty()) {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        } else {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        finish()
+        Handler().postDelayed({
+            if (Utils.loadPreferenceString(this, getString(R.string.token)).isNotEmpty()) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            finish()
+        }, 3000)
     }
 }
